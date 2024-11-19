3374ac418316c281c13e6ca0fa50ed6cce69a7ca
Prabhu kumar <prabhukumarmb@gmail.com>
2024-11-19 21:03:44 +0530

    created addTrust funtion to register the trust

--- Controllers/Trust.controller.js
+++ Controllers/Trust.controller.js
@@ -1,21 +1,37 @@
 let Trust=require('../models/Trust.model')
 // console.log(Trusts)
-let addTrusts=async(req,res,next)=>{
+// Trust.syncIndexes()
+//   .then(() => console.log("Indexes synced successfully"))
+//   .catch(err => console.error("Error syncing indexes:", err));
+
+let addTrusts=async(req,res)=>{
     try{
-        let {name,email}=req.body
-        // console.log(name)
-        let trustCredentials=await Trust.create({name,email}) 
+        let {firstName,email, lastName, phone, password, confirmPassword,trustName,
+             trustId, address, trustEmail,trustPhoneNumber }=req.body
+             
+        if(phone.length!==10 && trustPhoneNumber.length!==10){
+            throw new Error("Mobile Number should be 10 digits long")
+        }
+
+        let isExists = await Trust.findOne({$or: [{email: email}, {phone: phone}, {trustId: trustId}, {trustEmail: trustEmail}, {trustPhoneNumber: trustPhoneNumber}]})
+        
+        if(isExists){
+            throw new Error("user is already exists")
+        }
+
+        let trustCredentials=await Trust.create({firstName,lastName,email,phone, password, confirmPassword,trustName, trustId, address, trustEmail,trustPhoneNumber }) 
         res.status(201).json({error:false,message:"Trust Added Successfully",data:trustCredentials})
     }
     catch(err){
         res.status(500).json({error:true,message:err.message})
     }
 }
+
 let getTrusts=async(req,res,next)=>{
     try{
 
-        let Trust=await Trust.find({})
-        res.status(201).json({error:false,message:"Trust Fetch succesfully",data:trustCredentials})
+        let trusts =await Trust.find({})
+        res.status(201).json({error:false,message:"Trust Fetch succesfully",data:trusts})
     }
     catch(err){
         res.status(500).json({error:true,message:err.message})
--- models/Trust.model.js
+++ models/Trust.model.js
@@ -1,12 +1,58 @@
 let mongoose= require("mongoose")
 let Trustschema= new mongoose.Schema({
-    name:{
+    firstName:{
         required:true,
         type:String
     },
+    lastName:{
+        type: String,
+    },
     email:{
         required:true,
+        type: String, 
+        unique: true
+    },
+    phone:{
+        required:true,
+        type: String,
+        // minlength:  [10, "should be atleast 10 digits long"],
+        // maxLength: [10, "maximum only 10 digits should be entered"],
+        unique: true
+    },
+    password:{
+        required:true,
+        type: String,
+        minlength:  [8, "should be atleast 8 characters long"],
+    },
+    confirmPassword:{
+        required:true,
+        type: String,
+        minlength: [8, "should be atleast 8 characters long"],
+    },
+    trustName:{
+        type: String,
+        required:true,
+    },
+    trustId:{
+        required:true,
+        unique: true,
+        type: String
+    },
+    trustEmail: {
+        unique:true,
+        required:true,
         type:String
+    },
+    address: {
+        required: true,
+        type: String
+    },
+    trustPhoneNumber:{
+        unique:true,
+        required: true,
+        type: String
     }
+},{
+    timestamps: true
 })
 module.exports= mongoose.model("Trust",Trustschema);
