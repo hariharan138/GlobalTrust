2d9939f54831e489de04a308d272d6717f105940
Prabhu kumar <prabhukumarmb@gmail.com>
2024-11-20 00:30:32 +0530

    creted userSchema model

---
+++ models/User.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('express')
+
+const userSchema = new mongoose.Schema({
+    Name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        required:true,
+        type: String,
+        minlength:  [8, "should be atleast 8 characters long"],
+   },
+    confirmPassword:{
+        required:true,
+        type: String,
+        minlength:  [8, "should be atleast 8 characters long"],
+   },
+    phone:{
+        required:true,
+        type: String,
+        minlength:  [10, "should be atleast 10 digits long"],
+        maxLength: [10, "maximum only 10 digits should be entered"],
+        unique: true
+    }
+})
+
+module.exports = mongoose.model("UserSchema", userSchema )
