8d097dd0f18f83543f2046ff64a706a78cf097a5
Prabhu kumar <prabhukumarmb@gmail.com>
2024-11-20 09:43:25 +0530

    created User.controller.js file

--- Controllers/User.controller.js
+++ Controllers/User.controller.js
@@ -1,2 +1,28 @@
 const express = require("express")
-const User = require('../models/User.model')
+const UserModel = require('../models/User.model')
+const bcrypt = require('bcrypt')
+let userRegistration = async(req, res)=>{
+
+    try{
+            let hashedPassword = await bcrypt.hash(password, 10)
+
+            let newUser = new UserModel({
+                Name, 
+                email,
+                phone, 
+                password: hashedPassword, 
+                confirmPassword: hashedPassword
+            })
+
+            await newUser.save()
+
+            res.status(200).json({msg: "User Added SuccessFully", data: newUser})
+    }
+    catch(err){
+        res.status(500).json({error:true,message:err.message})
+    }
+}
+
+module.exports= {
+    userRegistration
+}
