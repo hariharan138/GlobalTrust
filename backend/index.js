7fa5ddd7edd80f676ea7f71d9139701a11085d71
Prabhu kumar <prabhukumarmb@gmail.com>
2024-11-23 15:41:27 +0530

    added credentials in backend

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,8 @@ const cookieParser = require('cookie-parser')
 const cors = require("cors")
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:3000',
+    credentials: true
 }))
 
 app.use(cookieParser())
