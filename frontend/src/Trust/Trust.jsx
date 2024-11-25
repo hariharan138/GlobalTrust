e4419fcb71b285eb47df4174923732163c4d299c
Prabhu kumar <prabhukumarmb@gmail.com>
2024-11-25 19:08:23 +0530

    added description for trust and users search api

--- frontend/src/Trust/Trust.jsx
+++ frontend/src/Trust/Trust.jsx
@@ -5,10 +5,12 @@ const Trust = () => {
   let s = "http://localhost:4000/api/trust/searchuser?search="
 
   let searchUser = async  ()=>{
-    // to get all the user just remove page and limit query parameter and make the searh as empty 
+    // use placeholdrs when you wnat to give value to the field ${} and use `` instead of '' or ""
+    // http://localhost:4000/api/trust/getusers/:page/:limit // this api is to get all the users in DB first params is for page second is for limit
+    // in blow api to get all the user just remove page and limit query parameter and make the searh as empty 
     // eg: http://localhost:4000/api/trust/searchuser?search=
     // the above one give all the result
-    let {data} = await axios.get('http://localhost:4000/api/trust/searchuser?search=Ram&page=1&limit=2', {
+    let {data} = await axios.get(`http://localhost:4000/api/trust/searchuser?search=Ram&page=1&limit=2`, {
       withCredentials: true
     })
   }
