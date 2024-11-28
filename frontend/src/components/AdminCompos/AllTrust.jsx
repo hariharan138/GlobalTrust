ebd837dc3d9d90141b82b041a1942de9ef8e2d1b
Prabhu kumar <prabhukumarmb@gmail.com>
2024-11-28 22:54:34 +0530

    updated trust and user list in admin page

--- frontend/src/components/AdminCompos/AllTrust.jsx
+++ frontend/src/components/AdminCompos/AllTrust.jsx
@@ -119,7 +119,7 @@ const AllTrust = () => {
 
                         {!loading && !errorMessage && searchedResult.length>0 && searchedResult.map(({trustName, _id, trustEmail, role, address, trustPhoneNumber, image}) => {
                             return (              
-                        <DisplaygetData setSearchedResult={setSearchedResult} searchedResult={searchedResult}  key={_id} _id={_id} Name={trustName} email={trustEmail} address={address} role={role} image={image} phone={trustPhoneNumber} />
+                            <DisplaygetData setSearchedResult={setSearchedResult} searchedResult={searchedResult}  key={_id} _id={_id} Name={trustName} email={trustEmail} address={address} role={role} image={image} phone={trustPhoneNumber} />
                             )
                         })
                         }
--- frontend/src/components/AdminCompos/DisplaygetData.jsx
+++ frontend/src/components/AdminCompos/DisplaygetData.jsx
@@ -3,18 +3,15 @@ import { Button } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import axios from 'axios';
-
+import dd from './Displaydata.module.css'
 
 const DisplaygetData = ({ _id, Name, email, address, phone, image, role, searchedResult, setSearchedResult}) => {
 
   const [userDeleted, setUserDeleted] = useState(null)
   const [deleteErrorMsg, setDeleteErrorMsg] = useState(null)
-// dummy data userDeleted={
-  // email: "lasjd@gmail.com",
-  // role: "trsut"}
+
  let deleteUser = async (userRole, id)=>{
-  // console.log(userRole)
-  // console.log(id) 
+
   try{
     let {data} = await axios.delete(`http://localhost:4000/api/admin/delete/${userRole}/${id}`, {
       withCredentials: true
@@ -29,39 +26,39 @@ const DisplaygetData = ({ _id, Name, email, address, phone, image, role, searche
 catch(err){
   console.log(err.response?.data?.message)
 }
-finally{
-  // console.log(userDeleted)
-}
-  
-
   
  }
 
   return (
-    <div className="card" key={_id}>
+     <div className={dd.card} key={_id}>
 
-                                        <div className="card-header">
-                                            <img src={image.url!="N/A" ? image.url: "https://th.bing.com/th/id/OIP.5uW6Y-lSAO-logZJQosljAHaHa?w=188&h=188&c=7&r=0&o=5&dpr=1.6&pid=1.7"} alt="" style={{width: "100px", height: "100px"}}/>
-                                            <h3>{Name}</h3>
-                                        </div>
+          <div className={dd.imgContainer}>
+              <img src={image.url!="N/A" ? image.url: "https://th.bing.com/th/id/OIP.5uW6Y-lSAO-logZJQosljAHaHa?w=188&h=188&c=7&r=0&o=5&dpr=1.6&pid=1.7"} alt="" style={{width: "100px", height: "100px"}}/>
+          </div>
 
-                                        <div className="card-content">
-                                            <div className="card-value" style={{fontSize: "16px"}}>{address}</div>
-                                            <div className="card-value" style={{fontSize: "16px"}}>{email}</div>
-                                            <div className="card-value" style={{fontSize: "16px"}}>{phone}</div>
-                                            <p className="card-subtext">{phone}</p>
-                                            <IconButton 
-                                            aria-label="delete"
-                                            color='warning'
-                                            style={{fontSize: "20px", height: "35px", backgroundColor: "#cc9d70"}}
-                                            onClick={()=> deleteUser(role, _id)}
-                                            ><DeleteIcon /></IconButton>
-                                            
-                                            
-                                        </div>
+          <div className={dd.personalDetail}>
+              <p   className={dd.field}>Name: <span className={dd.name}>{Name}</span></p>
+              <div className={dd.field}>Phone: <span className={dd.phone}> {phone}</span> </div>
+              <div className={dd.field}>Address: <span className={dd.address}> {address}</span></div>
+          {/* <div className="card-content"> */}
+              {/* <p className="card-subtext">{phone}</p> */}
+              {/* <IconButton aria-label="delete" color='warning'
+              style={{fontSize: "20px", height: "35px", width: "35px", float: "right" ,backgroundColor: "#cc9d70"}}
+              >
+              <DeleteIcon />
+              </IconButton> */}
 
+              <Button variant="contained" 
+               color='error'
+               onClick={()=> deleteUser(role, _id)} 
+               sx={{width: "70%", margin: "10px auto"}}
+               startIcon={<DeleteIcon />}>
+  Delete
+</Button>
+          {/* </div> */}
+          </div>
 
-                                    </div>
+      </div>
   )
 }
 
