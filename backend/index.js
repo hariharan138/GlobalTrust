2a1ee13db5402eaadcb210380004f90ae370a0b8
Prabhu kumar <prabhukumarmb@gmail.com>
2024-12-07 23:44:08 +0530

    added env file url

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser')
 const cors = require("cors")
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin:  process.env.FRONTEND_URL ||'http://localhost:3000',
     credentials: true
 }))
 
